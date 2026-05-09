8a36dfeabac170b04627d67021a3f5dbff4b4e18
Andrew <asoborev@mail.ru>
2026-05-09 11:38:35 +0300

    реализован middleware Trace

--- internal/core/transport/http/middleware/common.go
+++ internal/core/transport/http/middleware/common.go
@@ -3,6 +3,7 @@ package http_core_middleware
 import (
 	"context"
 	"net/http"
+	"time"
 
 	core_logger "github.com/Andrew1996-la/MindTick/internal/core/logger"
 	core_http_response "github.com/Andrew1996-la/MindTick/internal/core/transport/http/response"
@@ -59,7 +60,7 @@ func Panic() Middleware {
 			logger := core_logger.FromLogger(ctx)
 
 			httpHandlerResponse := core_http_response.NewHTTPHandlerResponse(logger, w)
-			
+
 			defer func() {
 				if r := recover(); r != nil {
 					httpHandlerResponse.PanicResponse(
@@ -69,7 +70,31 @@ func Panic() Middleware {
 				}
 			}()
 
-			next.ServeHTTP(w, r)  
+			next.ServeHTTP(w, r)
 		})
 	}
-} 
+}
+
+func Trace() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			logger := core_logger.FromLogger(ctx)
+			rw := core_http_response.NewResponseWriter(w)
+
+			start := time.Now()
+			logger.Debug(
+				">>> incoming HTTP request",
+				zap.Time("time", start.UTC()),
+			)
+
+			next.ServeHTTP(rw, r)
+
+			logger.Debug(
+				"<<< done HTTP request",
+				zap.Int("status code", rw.GetStatusCodeOrPanic()),
+				zap.Duration("latency", time.Since(start)),
+			)
+		})
+	}
+}
