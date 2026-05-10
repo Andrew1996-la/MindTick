26b41fc0943e6460add516ae16b9f1b658db2352
Andrew <asoborev@mail.ru>
2026-05-10 23:50:37 +0300

    конфигурация server

---
+++ internal/core/transport/http/server/confing.go
@@ -0,0 +1,34 @@
+package core_http_server
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Addr            string        `envconfig:"ADDR" required:"true"`
+	ShutdownTimeout time.Duration `envconfig:"SHUTDOWN_TIMEOUT" required:"true"`
+}
+
+func newConfig() (Config, error) {
+	var config Config
+
+	if err := envconfig.Process("HTTP", &config); err != nil {
+		return Config{}, fmt.Errorf("process envconfig: %w", err)
+	}
+
+	return config, nil
+}
+
+func NewConfigMust() Config {
+	config, err := newConfig()
+
+	if err != nil {
+		err = fmt.Errorf("get http config: %w", err)
+		panic(err)
+	}
+
+	return config
+}
---
+++ internal/core/transport/http/server/server.go
@@ -0,0 +1,74 @@
+package core_http_server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	core_logger "github.com/Andrew1996-la/MindTick/internal/core/logger"
+	"go.uber.org/zap"
+)
+
+type HttpServer struct {
+	mux    *http.ServeMux
+	config Config
+	logger core_logger.Logger
+}
+
+func NewHttpServer(
+	config Config,
+	logger core_logger.Logger,
+) *HttpServer {
+	return &HttpServer{
+		mux:    http.NewServeMux(),
+		config: config,
+		logger: logger,
+	}
+}
+
+func (h *HttpServer) Run(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    h.config.Addr,
+		Handler: h.mux,
+	}
+
+	ch := make(chan error, 1)
+
+	go func() {
+		defer close(ch)
+
+		h.logger.Warn("http server started", zap.String("Addr", h.config.Addr))
+
+		err := server.ListenAndServe()
+
+		if !errors.Is(err, http.ErrServerClosed) {
+			ch <- err
+		}
+	}()
+
+	select {
+	case err := <-ch:
+		if err != nil {
+			return fmt.Errorf("listen and serve http: %w", err)
+		}
+	case <-ctx.Done():
+		h.logger.Warn("shutdown server...")
+
+		shutdownCtx, cancel := context.WithTimeout(
+			context.Background(),
+			h.config.ShutdownTimeout,
+		)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			_ = server.Close()
+ 
+			return fmt.Errorf("shutdown HTTP server: %w", err)
+		}
+
+		h.logger.Warn("HTTP server stopped")
+	}
+
+	return nil
+}
